7f47a868ba05130aa276bd4103f4990c7dc206cc
Njire Njeri <njirenjeri@gmail.com>
2024-12-16 18:20:48 +0300

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 const init = () => {
-  
+  const inputForm = document.querySelector("form");
+  inputForm.addEventListener('submit', e =>{
+    e.preventDefault();
+    const userInput = e.target.searchByID.value
+
+    fetch(`http://localhost:3000/movies/${userInput}`)
+    .then(response => response.json())
+    .then((data) => {
+        console.log(data)
+        const titleDisplay = document.querySelector('#movieDetails h4')
+        const summaryDisplay = document.querySelector('#movieDetails p')
+
+        titleDisplay.textContent = data.title
+        summaryDisplay.textContent = data.summary
+    })
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init);
